fa849b409e06017e0ed7dfefe2f2156811b7f912
jkm8369 <jkm8369@naver.com>
2025-05-07 19:36:55 +0900

    2025-05-07 19:36

---
+++ src/com/javaex/review/Ex06.java
@@ -0,0 +1,24 @@
+package com.javaex.review;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		
+		for(int i=0; i<c.length; i++) {
+			System.out.print(c[i]);
+		}
+		
+		System.out.println();
+		char c1[] = {'T','h','i','s',',','i','s',',','a',',','p','e','n','c','i','l'};
+		
+		for(int i=0; i<c1.length; i++) {
+			System.out.print(c1[i]);
+		}
+		
+		
+	}
+	
+	
+}
---
+++ src/com/javaex/review/Ex08.java
@@ -0,0 +1,16 @@
+package com.javaex.review;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
